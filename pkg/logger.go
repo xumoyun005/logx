975ff43588a9015c93fb6dff364365f0ea34fe22
xumoyun005 <xumoyddinxolmuminov858@gmail.com>
2025-04-18 14:59:46 +0500

    removed comments

--- pkg/logger.go
+++ pkg/logger.go
@@ -4,12 +4,10 @@ var defaultLogger = New()
 
 type Logger struct{}
 
-// New creates a new logger instance
 func New() *Logger {
 	return &Logger{}
 }
 
-// Instance methods
 func (l *Logger) WithField(key string, value interface{}) *Entry {
 	return NewEntry(l).WithField(key, value)
 }
@@ -18,7 +16,6 @@ func (l *Logger) WithFields(fields Fields) *Entry {
 	return NewEntry(l).WithFields(fields)
 }
 
-// Package-level functions
 func WithField(key string, value interface{}) *Entry {
 	return defaultLogger.WithField(key, value)
 }
